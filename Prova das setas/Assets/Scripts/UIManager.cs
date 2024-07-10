c2e020d22bafcb9f27238445c5e26bd9f4564b70
pssgiasmim <iasmimpassing@gmail.com>
2024-07-10 09:06:13 -0300

    Método AtualizarSetaS

--- Prova das setas/Assets/Scripts/UIManager.cs
+++ Prova das setas/Assets/Scripts/UIManager.cs
@@ -1,14 +1,11 @@
 using TMPro;
 using UnityEngine;
 using System.Collections;
-
 using System.Collections.Generic;
-
-using TMPro;
+using UnityEngine.UI;
 
 using UnityEngine;
 
-using UnityEngine.UI;
  
 public class UIManager : MonoBehaviour
 
@@ -16,14 +13,15 @@ public class UIManager : MonoBehaviour
 
     UIManager instance;
 
-    [SerializeField] Sprite[] sprites = new Sprite[4];
+    [SerializeField] Sprite[] sprites;
 
-    [SerializeField] Image[] imagens = new Image[4];
+    [SerializeField] Image[] imagens;
 
-    [SerializeField] TextMeshProUGUI textoDePontuacao;
+    [SerializeField] TextMeshProUGUI textoDePontuacao; //Para qualquer TextMeshProUgui, é necessário ter um TMPro la encima, é gerado automaticamente.
 
     [SerializeField] TextMeshProUGUI textoDoRelogio;
 
+    //Qualquer AWAKE precisa de um SINGLETON.
     #region Singleton
 
     private void Awake()
@@ -36,22 +34,36 @@ public class UIManager : MonoBehaviour
 
     #endregion
 
-    public void AtualizarSetas(KeyCode[] setas)
+    public void AtualizarSetas(KeyCode[] setas) //Dentro do array/vetor KeyCode, tem o direcionamento de várias setas.
 
     {
 
-        for (int i = 0; i < imagens.Length; i++)
+        for (int i = 0; i < imagens.Length; i++) //Serve para percorrer cada imagem de cada seta.
+        {  //Deve se comparar se as imagens das setas, são iguais as setas selecionadas. Se a imagem é seta para cima, a seta deve ser para cima.
 
-        {
-
-            if (i <= setas.Length)
-
+            if (i >= setas.Length)
             {
-
-               // Image[0] = sprites[0];
-
+                imagens[i].sprite = sprites[0]; //Aqui se altera a imagens[i] de sprite, para sprites[0]. //o [i] para saber qual repetição esta.
+            }
+            else if (setas[i] == KeyCode.DownArrow) //Tecla para baixo
+            {
+                imagens[i].sprite = sprites[1];
+            }
+            else if (setas[i] == KeyCode.UpArrow) //Tecla para cima
+            {
+                imagens[i].sprite = sprites[2];
+            }
+            else if (setas[i] == KeyCode.LeftArrow) //Tecla para a Esquerda
+            {
+                imagens[i].sprite = sprites[3];
+            }
+            else if (setas[i] == KeyCode.RightArrow) //Tecla para a Direita
+            {
+                imagens[i].sprite = sprites[4];
             }
 
+            //Alteração de cor da imagem
+            imagens[i].color = Color.white;
         }
 
     }
